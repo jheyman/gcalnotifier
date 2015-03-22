990db9e1262592a3d4987b8bd45fba8ae1f029dd
Julien Heyman <bidsomail at gmail.com>
2015-03-22 16:19:56 +0100

    reversed tts.sh minor change

--- tts.sh
+++ tts.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # the -ao option is added to explicitly specify which sound output to use, in case multiple DACs are present
-say() { local IFS=+;/usr/bin/mplayer -ao alsa:device=hw=1.0 -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?tl=fr&q=$*"; }
+ #e.g. : -ao alsa:device=hw=1.0
+say() { local IFS=+;/usr/bin/mplayer -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?tl=fr&q=$*"; }
 say $*
